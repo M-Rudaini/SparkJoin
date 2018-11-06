7503faf0f671852555f4a70d737e0e8ff1472dcb
M-Rudaini <mamrya@gmail.com>
2018-11-06 10:26:02 +0200

    adding comments

--- src/main/scala/InnerJoins/SCoreRDD/HPIJ_GBK.scala
+++ src/main/scala/InnerJoins/SCoreRDD/HPIJ_GBK.scala
@@ -1,98 +1,66 @@
 package InnerJoins.SCoreRDD
-
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark._
-//to Write time
-import ScalaWriter.Writer.Write
-
-// Initializing the Hash Partitioned Inner-Join Using Group By Key..
+//  Initializing the Hash Partitioned Inner-Join Using Group by Key.
 object HPIJ_GBK {
-  //Defining the main method..
+  // Defining the main method.
   def main(args: Array[String]): Unit = {
-    //Setting logining levels..
+    // Setting the java logger levels.
     Logger.getLogger("org").setLevel(Level.ERROR)
     Logger.getLogger("akka").setLevel(Level.ERROR)
-    //Creating the Spark Core Configuration..
-    val conf = new SparkConf().setMaster("local[*]")
+    // Creating the Spark Core Configuration.
+    val conf = new SparkConf()
       .setAppName("RDD_HPIJ_GBK")
-    //Creating Spark Context using Spark Core Config..
+    // Creating Spark Context using Spark Core Configuration.
     val sc =new SparkContext(conf)
-
-    //Current time point log..
-    val t0 = System.currentTimeMillis()
-    // -Load Users File
-    val InUserFile = sc.textFile(args(0))
-
-    // -Load Purchase File
-    val InPurchaseFile = sc.textFile(args(1))
-    val t1 = System.currentTimeMillis()
-
-    // -Filtering the Header Line
-    val URDD1 =  InUserFile.filter(!_.contains("User_ID"))
-
-    // -Mapping Remaining Lines
-    // --Splitting line Values
-    val URDD2 = URDD1.map(line => line.split(","))
-
-    // --Taking User ID value (index 0),
-    // --Concating First (index 1) and Last (index 2) names with Space between them
-    // -Ignoring unwanted values
-    val URDD3 = URDD2.map(Array => (Array(0), Array(1)+" "+Array(2)))
-
-    //Creating the Hash Partitioner that Creates 200 Partitions
+    // Creating the Hash Partitioner that Creates 200 Partitions
     val Hpart = new HashPartitioner(200)
-
-    //Partitioning the (key, value) mapped User RDD using the Hash Partitioner
-    val UserRDD = URDD3.partitionBy(Hpart).persist()
-    //Printing the first 10 lines..
-    UserRDD.take(10).foreach(println)
-
-    // -Filtering the Header Line
-    val PRDD1 = InPurchaseFile.filter(!_.contains("User_ID"))
-
-    // -Mapping Remaining Lines
-    // --Splitting line Values
-    val PRDD2 = PRDD1.map(line => line.split(","))
-
-    // --Taking User ID value (index 1),
-    // --Multiplying No of Units (index 3) by Unit Price (index 4)
-    val PRDD3 = PRDD2.map(Array => (Array(1), Array(3).toFloat*Array(4).toFloat))
-
-    //Partitioning the (key, value) mapped Purchase RDD using the Hash Partitioner
-    val PRDD4 = PRDD3.partitionBy(Hpart).persist()
-    // -Grouping by key (UserID)
-    val PRDD5 = PRDD4.groupByKey()
-
-    // -Keeping the key and sum all the grouped values.
-    // -Ignoring unwanted values
-    val PurchaseRDD = PRDD5.mapValues(values => values.sum)
-
-    //Printing the first 10 lines..
-    PurchaseRDD.take(10).foreach(println)
-
-    val t2 = System.currentTimeMillis()
-    //Joining the two RDDs...
-    val UserPurchase = UserRDD.join(PurchaseRDD)
-
-    //Printing the first 10 lines..
+    //  -Load Users File.
+    val InUserFile = sc.textFile(args(0))
+      //  -Filtering the Header Line.
+      .filter(!_.contains("User_ID"))
+      //  -Mapping Remaining Lines.
+      //  --Splitting line Values.
+      .map(line => line.split(","))
+      //  --Taking User ID value (index 0).
+      //  --Merging First Name (index 1), Space “”, and Last Name (index 2).
+      //  -Ignoring unwanted values.
+      .map(Array => (Array(0), Array(1)+" "+Array(2)))
+      // Partitioning the (k, v) mapped User RDD using the Hash Partitioner.
+      .partitionBy(Hpart)
+      .persist() //  Caching.
+    // Printing the first 10 lines.
+    InUserFile.take(10).foreach(println)
+    //  -Load Purchase File.
+    val InPurchaseFile = sc.textFile(args(1))
+      //  -Filtering the Header Line.
+      .filter(!_.contains("User_ID"))
+      //  -Mapping Remaining Lines.
+      //  --Splitting line Values.
+      .map(line => line.split(","))
+      //  --Taking User ID value (index 1).
+      //  --Multiplying No of Units (index 3) by Unit Price (index 4).
+      .map(Array => (Array(1), Array(3).toFloat*Array(4).toFloat))
+      // Partitioning the (k, v) mapped Purchase RDD using the Hash Partitioner.
+      .partitionBy(Hpart)
+       //  Grouping the mapped values by key (User ID).
+      .groupByKey()
+       //  Mapping grouped values to take their total sum.
+      .mapValues(values => values.sum)
+      .persist() //  Caching.
+    // Printing the first 10 lines.
+    InPurchaseFile.take(10).foreach(println)
+    // Joining the two RDDs.
+    val UserPurchase = InUserFile.join(InPurchaseFile)
+    //  Releasing the cache of used RDDs.
+    InUserFile.unpersist()
+    InPurchaseFile.unpersist()
+    // Printing the first 10 lines.
     UserPurchase.take(10).foreach(println)
-
-    val t3 = System.currentTimeMillis()
-
-    val t4 = System.currentTimeMillis()
-//    UserPurchase.coalesce(1).saveAsTextFile(args(2))
-
-    val t5 = System.currentTimeMillis()
-    val TRead = t1-t0
-    val TPre = t2-t1
-    val TJoin = t3-t2
-    val TPost = t4-t3
-    val TWrite = t5-t4
-    println("Read, Pre-Processing, Join, Post-Processing, Write")
-    val Line = TRead+","+TPre+","+TJoin+","+TPost+","+TWrite
-    println(Line)
-    Write(Line,"RDD.HPIJ.GBK.csv")
-
+    //  Saving the Joined Data into the storage.
+    UserPurchase.saveAsTextFile(args(2))
+    //  Stopping the Spark Context.
     sc.stop()
   }
 }
+
