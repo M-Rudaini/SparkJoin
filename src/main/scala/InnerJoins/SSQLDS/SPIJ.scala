c574f8b01fbd841ae731e865138166ade58de860
M-Rudaini <mamrya@gmail.com>
2018-11-06 10:41:49 +0200

    adding comments

--- src/main/scala/InnerJoins/SSQLDS/SPIJ.scala
+++ src/main/scala/InnerJoins/SSQLDS/SPIJ.scala
@@ -1,101 +1,65 @@
 package InnerJoins.SSQLDS
-
-import ScalaWriter.Writer.Write
 import org.apache.log4j._
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
-
-
+//  Initializing the DS Semi-Processed Inner-Join.
 object SPIJ {
-
-  //-Creating Users Case Class Definition...
-  case class Users(User_ID: Long, First_Name: String, Last_name: String, DOP: String,
-                   Address:String, City: String, State_Abbr: String, Zip_Code: String,
-                   Country: String, Phone_Number: String, User_Name: String,
-                   Password: String, Email: String)
-
-
-  //-Creating Purchases Case Class Definition...
-  case class Purchases(Invoice_ID: Long, User_ID: Long, Product: String,
-                       Unit_Amount: Int, Unit_Price: Float)
-
+  // -Creating Users Case Class Definition...
+  case class Users(User_ID: Long, First_Name: String, Last_name: String, DOP: String, Address:String, City: String, State_Abbr: String, Zip_Code: String, Country: String, Phone_Number: String, User_Name: String, Password: String, Email: String)
+  // -Creating Purchases Case Class Definition...
+  case class Purchases(Invoice_ID: Long, User_ID: Long, Product: String, Unit_Amount: Int, Unit_Price: Float)
+  //  Defining the main method.
   def main(args: Array[String]): Unit = {
-    //Setting log Levels...
+    // Setting the logger Levels...
     Logger.getLogger("org").setLevel(Level.ERROR)
     Logger.getLogger("akka").setLevel(Level.ERROR)
-
-    //intializing Spark Session
-    val ss = SparkSession.builder.master("local[2]")
+    // Initializing Spark Session
+    val ss = SparkSession
+      .builder
       .appName("DS_SPIJ")
       .getOrCreate()
-
-    //To use spark session implicit tools like the "$" operator...
+    // To use spark session implicit tools like the "$" operator...
     import ss.implicits._
-
-    //Importing Users and Purchases Schema...
+    // Importing Users and Purchases Schema...
     val UserSchema = Encoders.product[Users].schema
     val ProductsSchema = Encoders.product[Purchases].schema
-
-    //Current time point log..
-    val t0 = System.currentTimeMillis()
-
-    //loading Users Dataset using the Users Case Class and Schema...
-    val InUserDS= ss.read.option("header", "true").schema(UserSchema).csv(args(0)).as[Users]
-    InUserDS.show(10)
-    InUserDS.printSchema()
-
-    //loading Purchase Dataset using the Users Case Class and Schema...
-    val InPurchseDS = ss.read.option("header","true").schema(ProductsSchema).csv(args(1)).as[Purchases]
-    InPurchseDS.show(10)
-    InPurchseDS.printSchema()
-
-    val t1 = System.currentTimeMillis()
-
-    //-Select the Wanted Columns From the Users Dataset
-    //--Concatenating First and Last Names as "Full_Name"...
-    val UDS1 = InUserDS.select($"User_ID", concat($"First_Name", lit(" "), $"Last_name") as "Full_Name")
-    UDS1.show(1)
-    UDS1.printSchema()
-
-    //-Select the Wanted Columns From the Purchases Dataset
-    //--Multiplying unit amount and unit price as "Total_Price"...
-    val PDS1 = InPurchseDS.select($"User_ID", $"Unit_Amount" * $"Unit_Price" as "Total_Price")
-    PDS1.show(1)
-    PDS1.printSchema()
-
-    val t2 = System.currentTimeMillis()
-
-    // Joining the two DataFrames...
-    val UPDS1 = UDS1.join(PDS1,"User_ID")
-    UPDS1.show(10)
-    UPDS1.printSchema()
-
-    val t3 = System.currentTimeMillis()
-
-
-    //-Grouping by both User Id and the full name...
-    val UPDS2 = UPDS1.groupBy($"User_ID",$"Full_Name")
-
-    //-Aggrigating through the grouped values summing all of them as "Total_Purchase".
-    val UserPurchase =UPDS2.agg(sum($"Total_Price") as "Total_Purchase")
-
-    UserPurchase.show(1)
-    UserPurchase.printSchema()
-    val t4 = System.currentTimeMillis()
-
-    //    UserPurchase.coalesce(1).write.csv(args(2))
-
-    val t5 = System.currentTimeMillis()
-    val TRead = t1-t0
-    val TPre = t2-t1
-    val TJoin = t3-t2
-    val TPost = t4-t3
-    val TWrite = t5-t4
-    println("Read, Pre-Processing, Join, Post-Processing, Write")
-    val Line = TRead+","+TPre+","+TJoin+","+TPost+","+TWrite
-    println(Line)
-    Write(Line,"DS.SPIJ.csv")
-
+    // loading Users Dataset using the Users Case Class and Schema...
+    // Considering the header as Column Names that will be used later...
+    val InUserDS= ss.read.option("header", "true")
+     .schema(UserSchema)  // Schema to apply.
+     .csv(args(0)) // Source Connector.
+     .as[Users]  // Case Class to apply.
+      // -Select the Wanted Columns From the Users Dataset
+      // --Concatenating First and Last Names as "Full_Name"...
+      .select($"User_ID", concat($"First_Name", lit(" "), $"Last_name") as "Full_Name")
+      .persist()  // Caching the Dataset for further use.
+    InUserDS.show(10)  // Printing the first 10 tuples.
+    InUserDS.printSchema()  // Printing the Dataset Schema.
+    // loading Purchase Dataset using the Users Case Class and Schema...
+    // Considering the header as Column Names that will be used later...
+    val InPurchseDS = ss.read.option("header","true")
+     .schema(ProductsSchema)  // Schema to apply.
+     .csv(args(1))  // Source Connector.
+     .as[Purchases]  // Case Class to apply.
+     // -Select the Wanted Columns From the Purchases Dataset
+     // --Multiplying unit amount and unit price as "Total_Price"...
+     .select($"User_ID", $"Unit_Amount" * $"Unit_Price" as "Total_Price")
+     .persist() // Caching the Dataset for further use.
+    InPurchseDS.show(10)  // Printing the first 10 tuples.
+    InPurchseDS.printSchema()  // Printing the Dataset Schema.
+    //  Joining the two Datasets...
+    val UserPurchase = InUserDS.join(InPurchseDS,"User_ID").persist()
+    // -Grouping by both User Id and the full name...
+    .groupBy($"User_ID",$"Full_Name")
+    // -Aggregating and summing all grouped values as "Total_Purchase".
+    .agg(sum($"Total_Price") as "Total_Purchase")
+      .persist()  // Caching the Dataset for further use.
+    UserPurchase.show(10)  // Printing the first 10 tuples.
+    UserPurchase.printSchema()  // Printing the Dataset Schema.
+    // Saving the Dataset into storage.
+    UserPurchase.write.csv(args(2)+"/out.csv")
+    //  Stopping the Spark Session.
     ss.stop()
   }
 }
+
