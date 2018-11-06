44592f266194a2f5c85cc30c6414a58832e2cd19
M-Rudaini <mamrya@gmail.com>
2018-11-06 10:37:52 +0200

    adding comments

--- src/main/scala/InnerJoins/SSQLDF/HPIJ.scala
+++ src/main/scala/InnerJoins/SSQLDF/HPIJ.scala
@@ -1,82 +1,58 @@
 package InnerJoins.SSQLDF
-
 import org.apache.log4j._
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
-import ScalaWriter.Writer.Write //to Write time
-
-
+//  Initializing the DF Hash-Partitioned Inner-Join.
 object HPIJ {
-
+  //  Defining the main method.
   def main(args: Array[String]): Unit = {
-    //Setting log Levels...
+    // Setting the logger Levels...
     Logger.getLogger("org").setLevel(Level.ERROR)
     Logger.getLogger("akka").setLevel(Level.ERROR)
-
-    //intializing Spark Session
-    val ss = SparkSession.builder.master("local[2]")
+    // Initializing Spark Session
+    val ss = SparkSession
+      .builder
       .appName("DF_HPIJ")
       .getOrCreate()
-    //To use spark session implicit tools like the "$" operator...
+    // To use spark session implicit tools like the "$" operator...
     import ss.implicits._
-
-    //Current time point log..
-    val t0 = System.currentTimeMillis()
-
-    //loading Users DataFrame...
-    val InUserDF = ss.read.option("header","true").csv(args(0))
-
-    //loading Purchases DataFrame...
-    val InPurchseDF = ss.read.option("header","true").csv(args(1))
-
-    val t1 = System.currentTimeMillis()
-
-    //Hash Repartitioning...
-    val PUserDF = InUserDF.repartition(200, $"User_ID")
-
-    //Hash Repartitioning...
-    val PPurchaseDF = InPurchseDF.repartition(200, $"User_ID")
-
-    //Selecting the Required Columns from the Users DataFrame...
-    //Concatenate first and last names as new column "Full_Name"...
-    val UserDF = PUserDF.select($"User_ID", concat($"First_Name", lit(" "), $"Last_Name")as "Full_Name")
-
-    //Print the first 10 tuples...
-    UserDF.show(10)
-
-    //Selecting the Required Columns from the Users DataFrame...
-    // to not conflict with the one in the Users DataFrame...
-    val PDF1 = PPurchaseDF.select($"User_ID", $"Unit_Amount" * $"Unit_Price" as "Total_Price")
-
-
-    val PDF2 = PDF1.groupBy("User_ID")
-    val PurchaseDF = PDF2.agg(sum("Total_Price") as "Total_Purchase")
-    //Print the first 10 tuples...
-    PurchaseDF.show(10)
-
-    val t2 = System.currentTimeMillis()
-
-    // Joining the two DataFrames...
-    val UserPurchase = UserDF.join(PurchaseDF, "User_ID")
+    // loading Users DataFrame...
+    // Considering the header as Column Names that will be used later...
+    val InUserDF = ss.read.option("header","true")
+     .csv(args(0)) // Source Connector.
+      // Selecting the Required Columns from the Users DataFrame...
+      // Concatenate first and last names as new column "Full_Name"...
+      .select($"User_ID", concat($"First_Name", lit(" "), $"Last_Name")as "Full_Name")
+      // Hash Repartitioning the selected data by User ID into 200 Blocks...
+      .repartition(200, $"User_ID")
+      .persist()    // Caching the repartitioned data for further use.
+    // Print the first 10 tuples...
+    InUserDF.show(10)
+    // loading Purchases DataFrame...
+    // Considering the header as Column Names that will be used later...
+    val InPurchseDF = ss.read.option("header","true")
+     .csv(args(1))  // Source Connector.
+      // Selecting the Required Columns from the Users DataFrame...
+      //  to not conflict with the one in the Users DataFrame...
+      .select($"User_ID", $"Unit_Amount" * $"Unit_Price" as "Total_Price")
+      // Hash Repartitioning the read data by User ID into200 Blocks...
+      .repartition(200, $"User_ID")
+      // Grouping the repartitioned data by User ID..
+      .groupBy("User_ID")
+    //  Aggregating and Summing all grouped total prices as total purchases ...
+      .agg(sum("Total_Price") as "Total_Purchase")
+      .persist()  // Caching the grouped data for further use.
+    // Print the first 10 tuples...
+    InPurchseDF.show(10)
+    //  Joining the two DataFrames...
+    val UserPurchase = InUserDF.join(InPurchseDF, "User_ID")
+      .persist()   // Caching the repartitioned data for further use.
+    // Print the first 10 tuples...
     UserPurchase.show(10)
-
-    val t3 = System.currentTimeMillis()
-
-    val t4 = System.currentTimeMillis()
-
-    //    UserPurchase.coalesce(1).write.csv(args(2))
-
-    val t5 = System.currentTimeMillis()
-    val TRead = t1-t0
-    val TPre = t2-t1
-    val TJoin = t3-t2
-    val TPost = t4-t3
-    val TWrite = t5-t4
-    println("Read, Pre-Processing, Join, Post-Processing, Write")
-    val Line = TRead+","+TPre+","+TJoin+","+TPost+","+TWrite
-    println(Line)
-    Write(Line,"DF.HPIJ.csv")
-
+    // Saving the joined Data...
+    UserPurchase.write.csv(args(2)+"/out.csv")
+    // Stopping the Spark Session...
     ss.stop()
   }
 }
+
